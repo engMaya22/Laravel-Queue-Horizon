e82fa2cd2c6e41b51a77caab484dc31895f4936c
maya <maya.e@webased.net>
2024-11-01 10:41:22 -0700

    jobs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\SendEmailJob;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/send-email', function(){
+    $email= 'eng.maya.esmaeel1@gmail.com';
+    SendEmailJob::dispatch($email);
+    return 'Email dispatched';
 });
